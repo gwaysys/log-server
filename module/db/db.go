58440976f8bc44524d7e7793b54ea9c10ef88e37
free1139 <free1139@163.com>
2023-12-05 06:23:17 +0000

    rename database to qsql

--- module/db/db.go
+++ module/db/db.go
@@ -3,19 +3,19 @@ package db
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gwaylib/conf"
-	"github.com/gwaylib/database"
+	"github.com/gwaylib/qsql"
 )
 
 var dbFile = conf.RootDir() + "/etc/db.cfg"
 
-func GetCache(section string) *database.DB {
-	return database.GetCache(dbFile, section)
+func GetCache(section string) *qsql.DB {
+	return qsql.GetCache(dbFile, section)
 }
 
-func HasCache(section string) (*database.DB, error) {
-	return database.HasCache(dbFile, section)
+func HasCache(section string) (*qsql.DB, error) {
+	return qsql.HasCache(dbFile, section)
 }
 
 func CloseCache() {
-	database.CloseCache()
+	qsql.CloseCache()
 }
